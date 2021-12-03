eab43c519d1a9b782ad00096c33ede19704f251e
p19609 <dawid.kapka@css.ch>
2021-12-03 10:24:42 +0100

    create CreateCardList method

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
 int main() {
-
+    Card* pCardList = CreateCardList(10);
     return 0;
 }
 
+// Card-Struktur, Dawid
 typedef struct Card {
+    int number;
     char name[50];
     int value1;
     double value2;
     struct Card* pNext;
 } Card;
+
+Card* CreateCardList(int cardCount) {
+    Card* pFirst = NULL;
+    Card* pNext = NULL;
+
+    for(int el = 0; el < cardCount; el++) {
+        Card* pNew = (Card*)malloc(sizeof(Card));
+        pNew->number = el + 1;
+        pNew->pNext = NULL;
+
+        if (pFirst == NULL) pFirst = pNew;
+        if (pLast != NULL) pLast->pNext = pNew;
+        pLast = pNew;
+    }
+    return pFirst;
+}
+
