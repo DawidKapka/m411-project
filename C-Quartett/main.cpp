dad1dd5ada465580f1df32349b1d0b62da5f46ac
DawidKapka <kapkadawid6@gmail.com>
2022-01-03 21:24:47 +0100

    aaaaaaaaaaaaaaa

--- C-Quartett/main.cpp
+++ C-Quartett/main.cpp
@@ -12,15 +12,18 @@ typedef struct Card {
     int value1;
     double value2;
     struct Card* pNext;
+    bool ownedByPlayer;
 } sCard;
 
 sCard* CreateCardList(int);
 void MixList(sCard*);
 void SwapCard(sCard*, sCard*, int);
 void StartGame(sCard*);
-sCard* GetCurrentPcCard(sCard*, int);
+sCard* GetNextCard(bool, sCard*, sCard*);
 void OutputCardFormatted(sCard*);
 void OutputTitle();
+bool ProcessAnswer(sCard*, sCard*, int*);
+bool CheckLoss(sCard*, sCard*);
 
 int main() {
     srand(time(0));
@@ -49,8 +52,8 @@ void StartGame(sCard* pFirst) {
     int playerCards = 5;
     int pcCards = 5;
     short round = 1;
-    sCard* pCurrentPlayerCard = pFirst;
-    sCard* pCurrentPcCard = GetCurrentPcCard(pFirst, playerCards);
+    sCard* pCurrentPlayerCard = GetNextCard(true, pFirst, pFirst);
+    sCard* pCurrentPcCard = GetNextCard(false, pFirst, pFirst);
     OutputTitle();
     while (playerCards > 0 && pcCards > 0)
     {
@@ -61,27 +64,29 @@ void StartGame(sCard* pFirst) {
         printf("************\n\n");
         printf("Deine Karte:\n\n");
         OutputCardFormatted(pCurrentPlayerCard);
-        printf("Pc Karte:\n\n");
-        OutputCardFormatted(pCurrentPcCard);
-        printf("Welchen Wert willst du spielen? (1/2): ");
+        printf("Welchen Wert willst du spielen? (1/2): ");        
         scanf_s("%i", &answer);
         if (answer >= 0 && answer <= 2)
         {
-            
-            bool roundWon = false;
-            if (answer == 1)
+            bool roundWon = ProcessAnswer(pCurrentPlayerCard, pCurrentPcCard, &answer);
+            if (roundWon)
             {
-                if (pCurrentPlayerCard->value1 > pCurrentPcCard->value1) roundWon = true;
-            }
-            else
-            {
-                if (pCurrentPlayerCard->value2 > pCurrentPcCard->value2) roundWon = true;
-                {
 
-                }
+                pCurrentPcCard->ownedByPlayer = true;
+                pCurrentPlayerCard = pCurrentPlayerCard->pNext;
             }
-            printf("%i", roundWon);
+            else {
+                pCurrentPlayerCard->ownedByPlayer = false;
+                pCurrentPcCard = pCurrentPcCard->pNext;
+            }
             round++;
+            pCurrentPlayerCard = GetNextCard(true, pFirst, pCurrentPlayerCard);
+            pCurrentPcCard = GetNextCard(false, pFirst, pCurrentPcCard);
+            bool gameEnded = CheckLoss(pCurrentPlayerCard, pCurrentPcCard);
+            if (gameEnded)
+            {
+                break;
+            }
         }
         else {
             printf("\nUng\x81ultige Eingabe!\n");
@@ -91,8 +96,34 @@ void StartGame(sCard* pFirst) {
 
 }
 
+bool CheckLoss(sCard* pPlayer, sCard* pPc) {
+    if (pPlayer == NULL)
+    {
+        printf("\n\nGame Over! Du hast verloren! :(\n\n");
+        return true;
+    }
+    else if (pPc == NULL)
+    {
+        printf("\n\nGratulation! Du hast gewonnen! :D\n\n");
+        return true;
+    }
+    return false;
+}
 
-
+// Werte vergleichen & auswerten - Dawid
+bool ProcessAnswer(sCard* pCurrentPlayerCard, sCard* pCurrentPcCard, int* answer) {
+    bool roundWon = false;
+    if (*answer == 1)
+    {
+        if (pCurrentPlayerCard->value1 > pCurrentPcCard->value1) roundWon = true;
+    }
+    else
+    {
+        if (pCurrentPlayerCard->value2 > pCurrentPcCard->value2) roundWon = true;
+    }
+    return roundWon;
+    
+}
 // Karte Formatiert in der Console ausgeben - Dawid
 void OutputCardFormatted(sCard* pCard) {
     printf("+--------Karte--------+\n");
@@ -100,18 +131,22 @@ void OutputCardFormatted(sCard* pCard) {
     puts(pCard->name);
     printf("| Value 1: %i\n", pCard->value1);
     printf("| Value 2: %lf\n", pCard->value2);
+    printf("owned by player: %i", pCard->ownedByPlayer);
     printf("+---------------------+\n\n");
 }
 
 // Erste Karte vom PC bestimmen - Noah
-sCard* GetCurrentPcCard(sCard* pFirst, int amount) {
-    sCard* pCurrentPcCard = pFirst;
-    for (int i = 0; i < amount; i++)
+sCard* GetNextCard(bool isPlayer, sCard* pFirst, sCard* pCurrentCard) {
+    bool secondRound = false;
+    for (sCard* pCurrent = pCurrentCard->pNext; pCurrent != NULL; pCurrent = pCurrent->pNext)
     {
+        if (pCurrent->ownedByPlayer == isPlayer)
+        {
+            return pCurrent;
+        }
 
-        pCurrentPcCard = pCurrentPcCard->pNext;
     }
-    return pCurrentPcCard;
+    return NULL;
 }
 
 // Karten-Liste erstellen - Noah
@@ -128,6 +163,7 @@ sCard* CreateCardList(int cardCount) {
         strcpy_s(pNew->name, "Test");
         pNew->value1 = value1[el];
         pNew->value2 = value2[el];
+        pNew->ownedByPlayer = (el + 1) % 2 == 0;
         pNew->pNext = NULL;
         if (pFirst == NULL) pFirst = pNew;
         if (pLast != NULL) pLast->pNext = pNew;
@@ -172,5 +208,3 @@ void SwapCard(sCard* pFirst, sCard* pSource, int number) {
 
     }
 }
-
-
